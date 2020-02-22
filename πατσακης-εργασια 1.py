64563c02430566e9cb36a635063be563171a9c2b
nikosf123 <61214255+nikosf123@users.noreply.github.com>
2020-02-22 18:46:41 +0200

    Update πατσακης-εργασια 1.py

--- πατσακης-εργασια 1.py
+++ πατσακης-εργασια 1.py
@@ -7,110 +7,35 @@ while epil !="1" and epil!="2":
   epil=raw_input("dwse epilogh  ")
 if epil=="1":
   arxeio=raw_input("dwse ena keimeno ")
-  le=[]
-  meg=[]  
   fin =open("test.txt","w")
   fin.write(arxeio)
   fin.close()
-  fin=open("test.txt","r")
-  w=fin.read()
-  fin.close 
-  word=""
-  gram=0
-  n=len(w)
-  sm=",,.,;,!,:,-,?,/,[,],(,),%,&,@,#,$,*,+"
-  for i in range (n) :
-    if w[i] !=" " and w[i] !="\n" and w[i] not in sm:
-      word=word+w[i]
-      gram=gram+1
-    else:
-      le.append(word)
-      meg.append(gram)
-      gram=0
-      word=""
-  le.append(word)
-  meg.append(gram)
-  plithos=0
-  pl=0
-  nub="0123456789"
-  for i in meg:
-      if i>1:
-        plithos=plithos+1
-  for i in le:
-    for k in i:
-     if k in nub:
-      pl=pl+1       
-  while len(le) < 5 or plithos==0 or pl>0:
-    if len(le)<5 and pl==0:
-      print "to keimeno poy edwses den exei 5 lejeis janadwse"
-      print"-------------------------------------------"
-    if plithos ==0 and pl==0:
-      print "to keimeno poy edwses den exei kamia leji megalyteri apo ena xaraktira janadwse"
-      print"-------------------------------------------"
-    if pl>0:
-      print "den mporei to keimeno na periexei arithmoys" 
-      print"-------------------------------------------" 
-    arxeio=raw_input("dwse ena keimeno ")  
-    fin =open("test.txt","w")
-    fin.write(arxeio)
-    fin.close()
-    fin=open("test.txt","r")
-    w=fin.read()
-    fin.close 
-    le=[]
-    meg=[]
-    word=""
-    gram=0
-    n=len(w)
-    sm=",,.,;,!,:,-,?,/,[,],(,),%,&,@,#,$,*,+"
-    for i in range (n) :
-      if w[i] !=" " and w[i] !="\n" and w[i] not in sm:
-        word=word+w[i]
-        gram=gram+1
-      else:
-        le.append(word)
-        meg.append(gram)
-        gram=0
-        word=""
-    le.append(word)
-    meg.append(gram)
-    r=len(le)
-    plithos=0
-    pl=0
-    nub="0123456789"
-    for i in meg:
-      if i>1:
-        plithos=plithos+1
-    for i in le:
-      for k in i:
-        if k in nub:
-          pl=pl+1    
 else:
   fin =open('test.txt', 'w')
   fin.write("simera einai deutera \n")
   fin.write("exoume mathima \n")
   fin.write("alla variemai na pao")
   fin.close() 
-  fin=open("test.txt","r")
-  w=fin.read()
-  fin.close     
-  le=[]
-  word=""
-  meg=[]
-  gram=0
-  n=len(w)
-  sm=",,.,;,!,:,-,_,?,/,[,],(,),%,&,@,#,$,*,+"
-  for i in range (n) :
-    if w[i] !=" " and w[i] !="\n" and w[i] not in sm:
-      word=word+w[i]
-      gram=gram+1
-    else:
-      le.append(word)
-      meg.append(gram)
-      gram=0
-      word=""
-  le.append(word)
-  meg.append(gram)
+fin=open("test.txt","r")
+w=fin.read()
+fin.close     
+le=[]
+word=""
+meg=[]
+gram=0
+n=len(w)
+sm="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in range (n) :
+  if w[i] !=" " and w[i] !="\n" and w[i] in sm:
+    word=word+w[i]
+    gram=gram+1
+  else:
+    le.append(word)
+    meg.append(gram)
+    gram=0
+    word=""
+le.append(word)
+meg.append(gram)
 print "to keimeno poy edwses"  
 print w
 print"-------------------------------------------"
@@ -119,7 +44,7 @@ for i in range(n-1):
   for k in range(n-1,i,-1):
     if meg[k]>meg[k-1]:
       meg[k],meg[k-1] = meg[k-1],meg[k]
-      le[k],le[k-1] = le[k-1],le[k]      
+      le[k],le[k-1] = le[k-1],le[k]
 print "oi pente megalyteres lejeis einai"
 xwris=""
 fon="aeuioyAEUIOY"
@@ -146,5 +71,5 @@ for i in range(5):
   teliko=teliko+nea+" "
   nea=""
 print"-------------------------------------------"  
-print "oi pente megalyters lejeis xwris ta fonienta kai antestramenes einai"  
-print (teliko)
+print "oi pente megalyters lejeis xwris ta\n fonienta kai antestramenes einai"  
+print teliko
